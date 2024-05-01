7d4793dcc09f448fe21722b02fcb8a1ed315a8a7
MattiMBP-Ubuntu <matti.rundelin@gmail.com>
2024-05-01 11:59:13 +0300

    First HashMap done. Committing and pushingn to test if the repo would get .git folder. Somehow I've managed to push so when I'm cloning I won't get my git -history.

--- mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -3,8 +3,22 @@ import java.util.HashMap;
 
 public class Nicknames {
 
+    /*
+     * 
+     * matthew's nickname is matt
+     * michael's nickname is mix
+     * arthur's nickname is artie
+     * 
+     */
+
     public static void main(String[] args) {
-        // Do the operations required here!
+        HashMap<String, String> nicknames = new HashMap<>();
+
+        nicknames.put("matthew", "matt");
+        nicknames.put("michael", "mix");
+        nicknames.put("arthur", "artie");
+
+        System.out.println(nicknames.get("matthew"));
     }
 
 }
